3e7cfd0bc428f706fd2e64e948b6b975eaf99b1d
Jackie Noel <94089894+JackieNoel@users.noreply.github.com>
2023-01-02 17:18:24 -0600

    Add files via upload

---
+++ words.py
@@ -0,0 +1,2 @@
+word_list = ["apple", "wares", "soup", "mount", "extend", "brown", "expert", "tired", "humidity", "backpack", "crust", "dent", "market", "knock",
+             "smile", "windy", "coin", "throw", "banana", "silence", "bluff", "climb", "dog", "candle", "marker", "pencil", "orange", "maid", "piano", "laundry"]
